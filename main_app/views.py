4cea855a77098005460d63a165132bcea3347a71
Bear Ranasinghe <bear.ranasinghe@gmail.com>
2020-12-14 23:11:00 -0500

    defined Bands model

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+class Band:
+    def __init__(self, name, genre, firstalbum):
+        self.name = name,
+        self.genre = genre,
+        self.favealbum = favealbum
+
+bands = [
+    Band('Megadeth', 'Speed Metal', 'Rust In Peace'),
+    Band('Metallica', 'Thrash Metal', 'Ride the Lightning'),
+    Band('Pallbearer', 'Sludge Metal', 'Foundations of Burden')
+]
+
 # Create your views here.
 
 from django.http import HttpResponse
@@ -9,3 +21,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def bands(request):
+  return render(request, 'bands/index.html', { 'bands': bands })
