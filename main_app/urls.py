6da00f180db6a01b0ce31549f93263d429bee815
Bear Ranasinghe <bear.ranasinghe@gmail.com>
2020-12-15 22:49:16 -0500

    mvp details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('bands/', views.bands_index, name='index'),
-  path('bands/<int:cat_id>/', views.bands_detail, name='detail')
+  path('bands/<int:band_id>/', views.bands_detail, name='detail')
 ]
 
