5a8b98c2b93bbf5d515250ada7b37c4bdd62b067
Bear Ranasinghe <bear.ranasinghe@gmail.com>
2020-12-15 22:14:46 -0500

    defined Bands model in Models module

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Band(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    favealbum = models.CharField(max_length=100)
+
+
