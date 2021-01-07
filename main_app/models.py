62f25147dcd68f3fe1f75519ffb163359108b23d
Bear <bear.ranasinghe@gmail.com>
2021-01-06 22:16:05 -0500

    imported User django model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -27,6 +28,7 @@ class Band(models.Model):
     genre = models.CharField(max_length=100)
     favealbum = models.CharField('Favourite Album', max_length=100)
     instruments = models.ManyToManyField(Instrument)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
